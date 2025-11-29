550bde562b6ca9c32bc7f3f5825b873c4cc7cfff
Enda Mullally <enda.mullally@outlook.com>
2025-11-29 17:13:59 +0000

    Pipeline tweaks

--- src/EM.Hasher.Tests/GlobalSuppressions.cs
+++ src/EM.Hasher.Tests/GlobalSuppressions.cs
@@ -6,4 +6,11 @@
 using System.Diagnostics.CodeAnalysis;
 
 [assembly: SuppressMessage("Interoperability", "CA1416:Validate platform compatibility", Justification = "<Pending>", Scope = "member", Target = "~M:EM.Hasher.Tests.Services.Settings.SettingsProviderTests.Settings_Bools_False_Works")]
+[assembly: SuppressMessage("Style", "IDE0161:Convert to file-scoped namespace", Justification = "<Pending>", Scope = "namespace", Target = "~N:EM.Hasher.Tests")]
+[assembly: SuppressMessage("Style", "IDE0161:Convert to file-scoped namespace", Justification = "<Pending>", Scope = "namespace", Target = "~N:EM.Hasher.Tests.Converters")]
+[assembly: SuppressMessage("Style", "IDE0161:Convert to file-scoped namespace", Justification = "<Pending>", Scope = "namespace", Target = "~N:EM.Hasher.Tests.Messages")]
+[assembly: SuppressMessage("Style", "IDE0161:Convert to file-scoped namespace", Justification = "<Pending>", Scope = "namespace", Target = "~N:EM.Hasher.Tests.Messages.UI")]
+[assembly: SuppressMessage("Style", "IDE0161:Convert to file-scoped namespace", Justification = "<Pending>", Scope = "namespace", Target = "~N:EM.Hasher.Tests.Models")]
+[assembly: SuppressMessage("Style", "IDE0161:Convert to file-scoped namespace", Justification = "<Pending>", Scope = "namespace", Target = "~N:EM.Hasher.Tests.Services.Application")]
 [assembly: SuppressMessage("Style", "IDE0161:Convert to file-scoped namespace", Justification = "<Pending>", Scope = "namespace", Target = "~N:EM.Hasher.Tests.Services.Hashes")]
+[assembly: SuppressMessage("Style", "IDE0161:Convert to file-scoped namespace", Justification = "<Pending>", Scope = "namespace", Target = "~N:EM.Hasher.Tests.Services.Settings")]
